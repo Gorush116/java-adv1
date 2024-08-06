be8d10f7e5bb4f4fdc002180913644b90c352eb1
Gorush116 <gorush34@gmail.com>
2024-08-06 09:39:46 +0900

    Thread 생성과 실행 commit

---
+++ src/thread/test/StartTestMain3.java
@@ -0,0 +1,23 @@
+package thread.test;
+
+import static util.MyLogger.log;
+
+public class StartTestMain3 {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 5; i++) {
+                    log("value: " + i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "counter");
+        thread.start();
+    }
+}
