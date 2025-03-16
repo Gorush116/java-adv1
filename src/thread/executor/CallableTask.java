075dc6f7fe5cadd8267fe7aa6b01b93315a74f60
Gorush116 <gorush34@gmail.com>
2025-03-17 08:37:07 +0900

    ExecutorService는 여러 ㅈ가업을 한 번에 편리하게 처리하는 invokeAll(), invokeAny() 기능을 제공한다.

---
+++ src/thread/executor/CallableTask.java
@@ -0,0 +1,11 @@
+package thread.executor;
+
+import java.util.concurrent.Callable;
+
+public class CallableTask implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
+    }
+}
